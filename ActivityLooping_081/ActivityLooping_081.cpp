23dc7a718dcf282d865b5e0e9b8e450d42957fbd
namira121 <127211008+namira121@users.noreply.github.com>
2023-03-22 17:40:39 +0700

    menambah syntax luaspersegipanjang

--- ActivityLooping_081/ActivityLooping_081.cpp
+++ ActivityLooping_081/ActivityLooping_081.cpp
@@ -1,20 +1,76 @@
-// ActivityLooping_081.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+float luaspersegipanjang(float p, float l) {
+	return p * l;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+float luaslingkaran(float r) {
+	return 3.14 * r * r;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+float luassegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+int main() {
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	cout << "\n\n==========================" << endl;
+	cout << "==========M E N U=========" << endl;
+	cout << "==========================" << endl;
+
+	cout << "1. luas persegi panjang" << endl;
+	cout << "2. luas segitiga" << endl;
+	cout << "3. luas lingkaran" << endl;
+	cout << "exit" << endl;
+
+	cout << "masukkan pilihan" << endl;
+	cout << "\npilihan (1/2/3/4) : ";
+	cin >> pilihan;
+
+
+
+
+	system("cls");
+
+	do
+	{
+
+
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukkan panjang : ";
+			cin >> panjang;
+			cout << "Masukkan lebar : ";
+			cin >> lebar;
+			cout << "Luas persegipanjang = " << luaspersegipanjang(panjang, lebar);
+			break;
+
+		case 2:
+			cout << "Masukkan alas : ";
+			cin >> alas;
+			cout << "Masukkan tinggi : ";
+			cin >> tinggi;
+			cout << "Luas Segitiga = " << luassegitiga(alas, tinggi);
+
+		case 3:
+			cout << "Masukkan Jari-jari : ";
+			cin >> jejari;
+			cout << "Luas lingkaran = " << luaslingkaran(jejari);
+			break;
+
+		case 4:
+
+			break;
+		deafult:
+			cout << "Pilihan anda salah!";
+			break;
+		}
+	} while (pilihan = 4);
+}
+
+
+
